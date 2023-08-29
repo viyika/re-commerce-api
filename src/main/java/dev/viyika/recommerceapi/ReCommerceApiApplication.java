9588d5f4b4fce0135587fa92951b8846472bb771
samitkumarpatel <samitkumarpatel@gmail.com>
2023-08-29 20:15:51 +0000

    first commit

---
+++ src/main/java/dev/viyika/recommerceapi/ReCommerceApiApplication.java
@@ -0,0 +1,13 @@
+package dev.viyika.recommerceapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCommerceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCommerceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/viyika/recommerceapi/ReCommerceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.viyika.recommerceapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCommerceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
